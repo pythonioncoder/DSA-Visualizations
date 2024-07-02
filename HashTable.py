9f789b04edc04f53bff1f23985db9c7d80347d9f
Vandit Rikhi <vanditrikhi@gmail.com>
2024-07-02 10:56:36 -0400

    Added Hash Tables

---
+++ HashTable.py
@@ -0,0 +1,48 @@
+'''
+Overview:
+Literally a Dictionary.
+Works by hashing keys.
+Hashing is running data through a hash function that returns a memory address.
+A hash is one-way and deterministic (same value is always the same hash)
+Collisions result in two pairs sharing the same position in a nested list or linked list
+	Called Seperate Chaining
+Another option is to keep going down until you find an empty space
+	Called Linear Probing (form of Open Addressing)
+'''
+
+
+class HashTable:
+	def __init__(self, size=7):
+		self.data_map = [None] * size
+
+	def __hash(self, key):
+		my_hash = 0
+		for letter in key:
+			my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+		return my_hash
+
+	def print_table(self):
+		for i, val in enumerate(self.data_map):
+			print(i, ": ", val)
+
+	def set_item(self, key, value):
+		index = self.__hash(key)
+		if self.data_map[index] is None:
+			self.data_map[index] = []
+		self.data_map[index].append((key, value))
+
+	def get(self, key):
+		index = self.__hash(key)
+		if self.data_map[index]:
+			for i in self.data_map[index]:
+				if i[0] == key:
+					return i[1]
+		return None
+
+	def keys(self):
+		all_keys = []
+		for i in self.data_map:
+			if i:
+				for j in i:
+					all_keys.append(j[0])
+		return all_keys
